272f0c119bc855651926ccd870d376e37465229c
jonn red <jonnred1123@gmail.com>
2020-11-24 20:14:05 +0800

    I solve the problem in z index but the design in cart section is not yet complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* cart section */
     const cartIcon = document.querySelector(".cart-icon");
     cartIcon.addEventListener("click" , () => {
-    cartIcon.parentElement.classList.toggle("cart")
+    cartIcon.parentElement.classList.toggle("cart");    
+    const cartSection = document.querySelector(".cart-section");
+    cartSection.classList.toggle("hide");
+    
 })
 

f766210e18e99b70d8e843f492641a150bebc24b
jonn red <jonnred1123@gmail.com>
2020-11-24 18:51:20 +0800

    I improve the cart button and make it toggle but there is z index bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* cart section */
     const cartIcon = document.querySelector(".cart-icon");
     cartIcon.addEventListener("click" , () => {
-    alert("asdas");
+    cartIcon.parentElement.classList.toggle("cart")
 })
 

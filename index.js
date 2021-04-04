da50f2287468d80fbe743a316e9419889d10d70c
jonn red <jonnred1123@gmail.com>
2021-04-04 21:40:24 +0800

    I completed the rows total price

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-/* cart section */
+/* cart icon section */
 const cartIcon = document.querySelector(".cart-icon");
 cartIcon.addEventListener("click" , () => {
     cartIcon.parentElement.classList.toggle("cart");    
     const cartSection = document.querySelector(".cart-section");
     cartSection.classList.toggle("hide");
 })
-
-
-
 // --------------remove 
 const removeBtnFunction = () =>{   
 const removeCartBtn = Array.from(document.querySelectorAll(".cart-remove-btn"));
@@ -15,11 +12,13 @@ removeCartBtn.forEach(element => {
     element.addEventListener("click" , (e) => {
         let product = element.parentElement.parentElement;
         product.remove();
+        // i still need to update the price
+        // need to update the total price and make the product show again
     })
 });
 }
 const addingCartToProducts = () =>{
-    const addToCart = Array.from(document.querySelectorAll(".add-to-cart"));
+    let addToCart = Array.from(document.querySelectorAll(".add-to-cart"));
     addToCart.forEach(element => {
         element.addEventListener("click" , (e) =>{
             let product = element.parentElement;
@@ -29,17 +28,12 @@ const addingCartToProducts = () =>{
             let price = productPrice[0].innerText;
             const productImg = product.querySelectorAll(".product-img")
             let image = productImg[0].src;
+            console.log("addingCart to producrs")
             addElementToCart(price ,image);
+            console.log("returned")
         });
     });
 }
-
-const totalPriceFunction = () => {
-    const totalPrice = document.querySelector(".total-price")
-    totalPrice.innerText 
-}
-
-
 
 const addElementToCart = (price , image) =>{
     let addItemToCart = `
@@ -50,26 +44,49 @@ const addElementToCart = (price , image) =>{
             <div class="price">${price}</div>
         </div>
         <input value="1" type="number" class="cart-product-quantity"></input>
-        <div class="cart-product-row-price cart-row">$100
+        <div class="cart-product-row-price"><span class="cart-row">${price}</span>
             <button class="cart-remove-btn">REMOVE</button>
         </div>
     </div>
     `;
     const cartContainer = document.querySelector(".cart-container");
     cartContainer.innerHTML += addItemToCart; 
+    console.log("addElement to =caart   ")
     removeBtnFunction();
+    PriceRowFunction();
+
+    return
 
 }
-const cartPriceXQuantity = () =>{
-    let cartProduct = document.querySelectorAll("cart-product")
-    for (let index = 0; index < cartProduct.length; index++) {
-        let cartPrice = document.querySelectorAll(".price")[index];
-        let quantity = document.querySelectorAll("cart-product-quantity")[index];
-        let result = cartPrice * quantity;
-        return result;
-    }
-        
+const PriceRowFunction = () => {
+    let cartQuantity = document.querySelectorAll('.cart-product-quantity');
+    for (let index = 0; index < cartQuantity.length; index++) {
+        let element = cartQuantity[index]
+        element.addEventListener("change", (e)=> {
+            console.log("iteratoi number:" + index)
+            let parent = element.parentElement;
+            let priceInt = parent.querySelectorAll(".price")[0].innerText;
+            priceInt = parseFloat(priceInt.replace('$' , ""));
+            let cartQuantity = parseFloat(element.value);
+            let result = cartRowTotal(cartQuantity , priceInt)
+            priceColumnFunction(result, index)
+            displayCartRowTotal(result, index)
+        });
+    }       
 }
+const priceColumnFunction = () => {
+    
+}
+const displayCartRowTotal = (result, index) =>{
+    console.log(index)
+    let totalprice = document.querySelectorAll('.cart-row')[index-1]
+    result = "$" +result
+    totalprice.innerText = result;
+    console.log(index)
+}
+
+
+
 // ------------------------- Purchaseeeee button 
  const purchaseFunction = () =>{
      const purchase = document.querySelector(".purchase-button");
@@ -77,10 +94,18 @@ const cartPriceXQuantity = () =>{
          alert("Thanks for buying!")
      })
  }
-//  --------------------------- Cart Total 
-
+//  --------------------------- Cart row Total 
+const cartRowTotal = (price , quantity ) => {return price * quantity}
 
 // ---------------functions 
+const start = () => {
+
+removeBtnFunction();
 addingCartToProducts();
 purchaseFunction();
-cartPriceXQuantity();
+PriceRowFunction();
+console.log("were aat START")
+
+}
+start();
+// https://ww7.readsnk.com/chapter/shingeki-no-kyojin-colored-chapter-119/
